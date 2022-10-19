bae19e63e98e30e8c1c7073783759045ee7d851e
imanishimwe adolphe <iadolphe007@gmail.com>
2022-10-19 07:29:27 +0200

    modification

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 
-let count = 0
-
+let count = 0;
 function increment(){
-    count ++
-let countElement = document.getElementById("counter").innerHTML = count
 
+    document.getElementById("decrement-btn").disabled = false;
+    count ++;
+    
+let countElement = document.getElementById("counter").innerHTML = count;
 }
-
 function decrement(){
-    if (count === 0) {
+    
+
+    if (count == 0){
         document.getElementById("decrement-btn").disabled = true;
     }
-    
     else {
         count --;    
     }
-let countElement = document.getElementById("counter").innerHTML = count
-
-
+    
+let countElement = document.getElementById("counter").innerHTML = count;
 }
